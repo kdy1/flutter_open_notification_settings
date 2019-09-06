402ef330e398a827cf9a079dfa7980990a81ca92
강동윤 <kdy1@outlook.kr>
2019-09-06 12:36:44 +0900

    Files for ide

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/kdy1/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kdy1/projects/open_noti_settings/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/kdy1/flutter/bin/cache/artifacts/engine/ios"
